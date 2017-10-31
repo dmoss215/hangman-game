424f1da014f11075848c340879e49c5b06f29b01
Daniel Moss <dmoss@stratoswp.com>
2017-10-30 20:25:34 -0400

    initial commit with basic html and css

---
+++ app.js
@@ -0,0 +1,7 @@
+var tvShows = ["Game of Thrones", "The Last Man on Earth", "Stranger Things", "Seinfeld", "Curb Your Enthusiasm", "The Big Bang Theory"]
+
+//Get the key pressed by the user
+document.onkeyup = function(event) {
+  var letterPressed = event.key;
+
+}
