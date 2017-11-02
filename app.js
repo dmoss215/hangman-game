700af6dca04cda491958e6a134160b660bfa2a06
Daniel Moss <dmoss@stratoswp.com>
2017-11-02 11:30:18 -0400

    all js working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+function newGame(){
+
 var techCo = ["amazon", "google", "microsoft", "twitter", "facebook", "instagram", "veeam", "cisco", "intel", "apple"]
 //Choose random string from tvShows array
 var randNum = Math.floor(Math.random() * techCo.length);
@@ -18,47 +20,42 @@ function createUnderscores() {
   }
 }
 
-createUnderscores();
+//createUnderscores();
+  createUnderscores();
 console.log(underscores);
 console.log(underscores.join(' '));
 
 //Start new game when new game button is pressed and insert underscores to represent the randCo string
-function newGame(){
+  document.getElementById('wrong-letters').innerHTML = "<p>  </p>"
   document.getElementById('randomShow').innerHTML = "<div>"+underscores.join(' ')+"</div>";
   document.getElementById('games-remaining').innerHTML = "<p>Guesses:</p>" + guessesRemaining;
 
+  //Get the key pressed by the user
+  document.onkeyup = function(event) {
+    var letterPressed = event.key;
 
+    console.log(letterPressed);
 
-}
+    //if the letter pressed matches a letter in lettersInCompany, replace the letter in underscores
 
-//Get the key pressed by the user
-document.onkeyup = function(event) {
-  var letterPressed = event.key;
-
-  console.log(letterPressed);
-
-  //if the letter pressed matches a letter in lettersInCompany, replace the letter in underscores
-
-  if (lettersInCompany.indexOf(letterPressed) > -1 ) {
-    for (var i = 0; i < underscores.length; i++) {
-      if (lettersInCompany[i] == letterPressed) {
-        underscores[i] = letterPressed;
-        document.getElementById('randomShow').innerHTML = underscores.join(' ');
+    if (lettersInCompany.indexOf(letterPressed) > -1 ) {
+      for (var i = 0; i < underscores.length; i++) {
+        if (lettersInCompany[i] == letterPressed) {
+          underscores[i] = letterPressed;
+          document.getElementById('randomShow').innerHTML = underscores.join(' ');
+        }
       }
+    } else {
+      guessesRemaining--;
+      wrongGuess.push(letterPressed);
+      document.getElementById('games-remaining').innerHTML = "<p>Guesses:</p>" + guessesRemaining;
+      document.getElementById('wrong-letters').innerHTML = wrongGuess
     }
-  } else {
-    guessesRemaining--;
-    wrongGuess.push(letterPressed);
-    document.getElementById('games-remaining').innerHTML = "<p>Guesses:</p>" + guessesRemaining;
-    document.getElementById('wrong-letters').innerHTML = wrongGuess
-  }
 
-  if (guessesRemaining == 0) {
-    document.getElementById('randomShow').innerHTML = "YOU LOSE!"
+    if (guessesRemaining == 0) {
+      document.getElementById('randomShow').innerHTML = "YOU LOSE!"
+    }
+
   }
 
 }
-
-
-// Insert item from tvShows array under "TV Show: in HTML"
-// document.getElementById('#').innerHTML = "";
